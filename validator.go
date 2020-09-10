e70cebc7e56b88cc5920279aada4b37493322464
Raphael Zöllner <raphael.zoellner@outlook.com>
2020-09-10 22:55:49 +0200

    Add NilValidations

--- validator.go
+++ validator.go
@@ -16,9 +16,9 @@ type Field struct {
 type CustomValidationFunc func(ctx context.Context, f Field) error
 
 type CustomValidator struct {
-	Validate CustomValidationFunc
-	TagRegex *regexp.Regexp
-	shouldFailIfNil bool
+	Validate        CustomValidationFunc
+	TagRegex        *regexp.Regexp
+	ShouldFailIfNil bool
 }
 
 type Validator struct {
@@ -30,19 +30,33 @@ func NewValidator() *Validator{
 }
 
 func (v *Validator) Validate(ctx context.Context, i interface{}) error{
-	iType := reflect.TypeOf(i)
 	iValue := reflect.ValueOf(i)
+	iType := iValue.Type()
 	kind := iType.Kind()
 
 	// if the kind of the provided interface is interface or pointer use its underlying element instead
 	if kind == reflect.Interface || kind == reflect.Ptr{
 		if iValue.IsNil() {
 			// TODO fail validators that should fail on a nil ptr
+			// fail validators that should fail on a nil ptr
+			iType = iType.Elem()
+			kind = iType.Kind()
+
+			if kind != reflect.Struct {
+				// if the kind is not struct there is nothing to be validated
+				return nil
+			}
+
+			err := v.failStructNilValidations(iType)
+			if err != nil {
+				return err
+			}
+
 			return nil
 		}
 
-		iType = iType.Elem()
 		iValue = iValue.Elem()
+		iType = iValue.Type()
 		kind = iType.Kind()
 	}
 
@@ -50,7 +64,7 @@ func (v *Validator) Validate(ctx context.Context, i interface{}) error{
 		return fmt.Errorf("validation of kind %v is not supported", kind)
 	}
 
-	err := v.validateStruct(ctx, iType, iValue)
+	err := v.validateStruct(ctx, iValue)
 	if err != nil{
 		return err
 	}
@@ -66,7 +80,10 @@ func (v *Validator) RegisterValidationFunc(cv CustomValidator){
 	v.CustomValidators[cv.TagRegex.String()] = cv
 }
 
-func (v *Validator) validateStruct(ctx context.Context, structType reflect.Type, structValue reflect.Value) error {
+
+// Should only be used on reflect.Values of kind struct
+func (v *Validator) validateStruct(ctx context.Context, structValue reflect.Value) error {
+	structType := structValue.Type()
 	for i:=0; i < structType.NumField(); i++ {
 		structField := structType.Field(i)
 		fieldValue := structValue.Field(i)
@@ -87,10 +104,10 @@ func (v *Validator) validateStruct(ctx context.Context, structType reflect.Type,
 
 func (v *Validator) validateField(ctx context.Context, field Field) error {
 	// Validate Field if it contains a subTag matching a regex of any custom validator
-	for _, cv := range v.CustomValidators {
-		validatorTag := field.structField.Tag.Get("validator")
-		subTags := strings.Split(validatorTag, ";")
+	validatorTag := field.structField.Tag.Get("validator")
+	subTags := strings.Split(validatorTag, ";")
 
+	for _, cv := range v.CustomValidators {
 		for _, subTag := range subTags{
 			if cv.TagRegex.MatchString(subTag) {
 				err := cv.Validate(ctx, field)
@@ -101,29 +118,91 @@ func (v *Validator) validateField(ctx context.Context, field Field) error {
 		}
 	}
 
-	fType := field.structField.Type
 	fValue := field.value
+	fType := fValue.Type()
 	kind := fType.Kind()
 
 
 	// if the kind of the field is interface or pointer use its underlying element instead
 	if kind == reflect.Interface || kind == reflect.Ptr{
 		if fValue.IsNil() {
-			// TODO fail validators that should fail on a nil ptr
+			// fail validators that should fail on a nil ptr
+			fType = fType.Elem()
+			kind = fType.Kind()
+
+			if kind != reflect.Struct {
+				// if the kind is not struct there is nothing to be validated
+				return nil
+			}
+
+			err := v.failStructNilValidations(fType)
+			if err != nil {
+				return err
+			}
+
 			return nil
 		}
 
-		fType = fType.Elem()
 		fValue = fValue.Elem()
+		fType = fValue.Type()
 		kind = fType.Kind()
 	}
 
+	// If the field is not of kind struct there is nothing to be validated anymore
+	if kind != reflect.Struct {
+		return nil
+	}
+
 	// If the field itself is of kind struct validate the nested struct
-	if kind == reflect.Struct {
-		err := v.validateStruct(ctx, fType, fValue)
-		if err != nil {
+	err := v.validateStruct(ctx, fValue)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *Validator) failStructNilValidations(structType reflect.Type) error {
+	for i:=0; i < structType.NumField(); i++ {
+		structField := structType.Field(i)
+
+		err := v.failStructFieldNilValidations(structField)
+		if err != nil{
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (v *Validator) failStructFieldNilValidations(structField reflect.StructField) error {
+	validatorTag := structField.Tag.Get("validator")
+	subTags := strings.Split(validatorTag, ";")
+
+	for _, cv := range v.CustomValidators{
+		for _, subTag := range subTags {
+			if cv.TagRegex.MatchString(subTag) && cv.ShouldFailIfNil {
+				return fmt.Errorf("validation failed since validator for regex: %v failed", cv.TagRegex.String())
+			}
+		}
+	}
+
+	fType := structField.Type
+	kind := fType.Kind()
+	if kind == reflect.Interface || kind == reflect.Ptr {
+		fType = fType.Elem()
+		kind = fType.Kind()
+	}
+
+	// If the field is not of kind struct there is nothing to be validated anymore
+	if kind != reflect.Struct {
+		return nil
+	}
+
+	// If the field itself is of kind struct validate the nested struct
+	err := v.failStructNilValidations(fType)
+	if err != nil {
+		return err
 	}
 
 	return nil
